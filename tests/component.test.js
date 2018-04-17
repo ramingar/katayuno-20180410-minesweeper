8be0920cd2fc491450e0691288880146a1e6061b
Rafael Minguet <rminguet@gmail.com>
2018-04-17 20:52:11 +0200

    Day 17-04-2018

--- tests/component.test.js
+++ tests/component.test.js
@@ -4,20 +4,37 @@ import test from 'tape';
 
 const Minesweeper = function ({width, height, initialMap = ''}) {
 
-    const MINE = '*';
+    const MINE     = '*';
+    let next_field = '.';
 
     const showMap = function () {
         const resumeMap = initialMap.split('').reduce(buildMap, '');
         return resumeMap;
     };
 
-    const buildMap = function (acc, field) {
+    const buildMap = function (acc, field, idx, arr) {
         if (isMine(field)) {
-            acc += MINE;
-        } else {
+            acc += manageMine();
+        }
+        else if (next_field === '1') {
+            acc += manageWarn();
+        }
+        else
+        {
             acc += '.';
         }
+
         return acc;
+    };
+
+    const manageMine = function () {
+        next_field = '1';
+        return MINE;
+    };
+
+    const manageWarn = function () {
+        next_field = '.';
+        return '1';
     };
 
     const isMine = function (field) {
@@ -58,5 +75,25 @@ test('minesweeper with a mine', (assert) => {
     const map = minesweeper.showMap();
 
     assert.equal(map, expected, 'Map must have one mine');
+    assert.end();
+});
+
+test('minesweeper with one mine and one warn', (assert) => {
+    const expected    = '*1';
+    const minesweeper = Minesweeper({width: 2, height: 1, initialMap: '*.'});
+
+    const map = minesweeper.showMap();
+
+    assert.equal(map, expected, 'Map must have one mine and maps warns how many mines there are');
+    assert.end();
+});
+
+test('minesweeper with one mine, one warn and an empty field', (assert) => {
+    const expected    = '*1.';
+    const minesweeper = Minesweeper({width: 3, height: 1, initialMap: '*..'});
+
+    const map = minesweeper.showMap();
+
+    assert.equal(map, expected, 'Map must have one mine and maps warns how many mines there are');
     assert.end();
 });
